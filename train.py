b4c07c1ce45e582e37605136f3253556df86386b
xuqi <welch_cn@163.com>
2022-09-14 14:52:19 +0800

    test

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
-import numpy
+import math
+import numpy as np
+import math
 
-if float("inf")!=float("inf"):
-    print("false")
-else:
-    print("true")
+a = np.array([1,2,3,4])
+b = np.array([4,3,2,1])
+print(a/b)
